f071ad0544dde3595c976a14559a6d43e251b270
shibasis <13601918+shibasis0801@users.noreply.github.com>
2023-09-19 11:41:29 +0530

    added flatbuffers, as we need to build it from source

--- core/droidMain/extensions/Activity.kt
+++ core/droidMain/extensions/Activity.kt
@@ -11,6 +11,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.registerForActivityResult
 import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
